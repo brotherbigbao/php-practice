1fc18766aa47bc56c5002d86908a2251ed5993eb
Yibao Liu <734988964@qq.com>
2018-02-27 14:16:09 +0800

    bitwise

--- operators/bit_wise.php
+++ operators/bit_wise.php
@@ -6,12 +6,22 @@
  * Time: 下午1:42
  */
 
+//右补0, 高位左移后溢出，舍弃。
+
 $a = 1;
+echo $a, ":", decbin($a), " =====> ";
 $a = $a << 2; //左移两位
 echo $a, ":", decbin($a), "\n"; //decbin()获取int类型的二进制表示字符串
 
-$a = ~ $a; //取反
-echo $a,":", decbin($a), "\n";
+$b = 5;
+echo $b, ":", decbin($b), " =====> ";
+$b = $b << 3;
+echo $b, ":", decbin($b), "\n";
+
+$d = 2;
+echo $d, ":", decbin($d), " =====> ";
+$d = ~ $d; //取反
+echo $d,":", decbin($d), "\n";
 
 /*The size of an integer is platform-dependent, although a maximum value of about two billion is the usual value (that's
  32 bits signed). 64-bit platforms usually have a maximum value of about 9E18, except on Windows prior to PHP 7, where
@@ -19,4 +29,4 @@ it was always 32 bit. PHP does not support unsigned integers. Integer size can b
  maximum value using the constant PHP_INT_MAX since PHP 5.0.5, and minimum value using the constant PHP_INT_MIN since PHP 7.0.0.*/
 
 //echo PHP_INI_SIZE;//php7
-echo PHP_INT_MAX;
+echo "PHP_INT_MAX:" , PHP_INT_MAX , ",", decbin(PHP_INT_MAX), ",", strlen(decbin(PHP_INT_MAX)), "\n";
