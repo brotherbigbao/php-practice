402302b988b8f1004759b8bb16b7a218a4144f42
Yibao Liu <734988964@qq.com>
2018-08-27 11:00:02 +0800

    update datetime

--- datetime/diff.php
+++ datetime/diff.php
@@ -8,3 +8,7 @@
 $before = new DateTime('2017-10-10');
 $today = new DateTime('2018-01-26');
 echo $today->diff($before)->days, "\n";
+
+$before = new DateTime('2018-06-30');
+$today = new DateTime('2020-08-31');
+echo $today->diff($before)->m, "\n"; //还是2，不是所有的月数，够12个月就加到年了
