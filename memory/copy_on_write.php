e4eeff6983da110874bd82f22f744a08ef0e3055
Yibao Liu <734988964@qq.com>
2019-09-19 14:59:30 +0800

    update

--- memory/copy_on_write.php
+++ memory/copy_on_write.php
@@ -1,1 +1,20 @@
 <?php
+function getA() {
+    $a = [];
+    for ($i=0; $i<1000000; $i++) {
+        $a[] = $i;
+    }
+    echo memory_get_usage(), "\n";
+    return $a;
+}
+
+$b = getA();
+echo memory_get_usage(), "\n";
+
+//$b = getA();
+//echo memory_get_usage(), "\n";
+
+//$b[0] = 999;
+//echo memory_get_usage(), "\n";
+
+
