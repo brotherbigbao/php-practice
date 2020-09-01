177db8578a59bcca5a5e2e562711f95d65cd2130
Yibao Liu <734988964@qq.com>
2020-09-01 14:00:37 +0800

    update

--- empty/empty1.php
+++ empty/empty1.php
@@ -2,12 +2,23 @@
 
 $data = ['name-1' => ['a' => 1, 'b' => 2, 'c' => 3]];
 $key = [
-    'name' => 'name-1',
+    'name' => 'name-w',
     'age' => 'age-1',
 ];
 
-echo ($data['name-1']['x'] ?? 'not found'), "\n\n";
-
-if (isset($data[$key['names']]['w'])) {
-    echo "is empty\n";
+//变量$key下标不存在时用isset()也会报错
+if (isset($data[$key['namex']]['w'])) {
+    echo "exist\n";
+} else {
+    echo "not found\n";
 }
+
+//$key下标存在，即使$data下标不存在也不会报错
+if (isset($data[$key['name']]['w'])) {
+    echo "exist\n";
+} else {
+    echo "not found\n";
+}
+
+
+//结论: isset(), ??, empty() 等操作符  只对主变量有效, 子变量下标不存在仍会报 "Undefined index"
