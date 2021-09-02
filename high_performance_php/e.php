39c766212c024506de07f00f86f37031a6b304fd
Yibao Liu <734988964@qq.com>
2021-09-02 11:41:13 +0800

    update

--- high_performance_php/e.php
+++ high_performance_php/e.php
@@ -2,9 +2,16 @@
 //僵尸进程的管理
 $i_pid = pcntl_fork();
 if (0 == $i_pid) {
-    //子进程10秒后退出
-    sleep(10);
+    //在子进程中
+    for($i=1; $i<=10; $i++) {
+        sleep(1);
+        echo '子进程PID' . posix_getpid() . '倒计时:' . $i . PHP_EOL;
+    }
 } else if ($i_pid > 0) {
-    //父进程1000秒后退出
-    sleep(1000);
+    $i_ret = pcntl_wait($status);
+    echo $i_ret . ' : ' . $status . PHP_EOL;
+    //while保持父进程不退出
+    while (true) {
+        sleep(1);
+    }
 }
