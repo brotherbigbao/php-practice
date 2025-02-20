7971e10d7d8e171a5802c4ddf726c2bd3a010bf3
liuyibao <liuyibao@styd.cn>
2025-02-20 10:27:20 +0800

    u

--- base64/base64_bin2hex.php
+++ base64/base64_bin2hex.php
@@ -27,3 +27,14 @@ $hex_escaped_str = string_to_hex_escape($original_str);
 
 echo "原始字符串: $original_str\n";
 echo "以 \\x 开头的十六进制转义序列: $hex_escaped_str\n";
+
+
+/**
+ * "name":"nameikaka","activity_type":0,"activity_id":0,"inviter_id":0,"country_code":"86"}},
+ * "error_code":256,"error_file":"\/export\/apps\/release\/astroforces_20250218100816\/vendor\/uto\/uto-sdk\/php\/UtoProto\/Member\/MemberInfo.php",
+ * "error_line":355,"error_msg":"Given string is not UTF8 encoded.",
+ * "error_trace":["#0 \/export\/apps\/release\/astroforces_20250218100816\/vendor\/uto\/uto-sdk\/php\/UtoProto\/Member\/MemberInfo.php(355):
+ * app\\components\\ErrorHandle->handleError(256, 'Given string is...', '\/export\/apps\/re...', 355, Array)",
+ * "#1 \/export\/apps\/release\/astroforces_20250218100816\/modules\/v1\/services\/member\/MemberService.php(1874):
+ * UtoProto\\Member\\MemberInfo->setMemberName('\\x9D\\xA9\\x9E\\x8AF\\xA4')"
+ */
