4b95f94240febd3795944c50f244fd4b44d47616
liuyibao <liuyibao@aliyun.com>
2025-06-20 17:19:45 +0800

    u

---
+++ hash_algo/sha256.php
@@ -0,0 +1,37 @@
+<?php
+
+function getAsciiUnitsPlace(string $char)
+{
+    return ord($char)%10;
+    //return ord($char) . ',';
+}
+
+$arr = [
+    '3243693956544536',
+    '3241832809571200',
+    '3228692575259681',
+    '3212755813974694',
+    '3204497179067660',
+    '3198862584587737',
+    '3197628217627190',
+    '3194493176720967',
+    '3194036165350205',
+    '3185274079759282',
+    '3176792358290666',
+    '3169802701571284',
+    '3165289898954828',
+];
+
+foreach ($arr as $i) {
+    $h = hash('sha256', $i);
+
+    echo getAsciiUnitsPlace($h[6])
+        . getAsciiUnitsPlace($h[9])
+        . getAsciiUnitsPlace($h[15])
+        . getAsciiUnitsPlace($h[44])
+        . getAsciiUnitsPlace($h[47])
+        . getAsciiUnitsPlace($h[55]);
+
+    echo "\n";
+}
+
